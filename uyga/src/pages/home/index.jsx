7d44c42acc4e7dde32c3eebf1ef277ab790d7418
Jahongirov01 <153820313+Jahongirov01@users.noreply.github.com>
2024-03-21 19:12:32 +0500

    Add files via upload

---
+++ uyga/src/pages/home/index.jsx
@@ -0,0 +1,200 @@
+import { useState } from "react";
+
+
+
+const HomeView = () => {
+  
+  return (
+    <>
+
+    <input type="text" placeholder="Search for movies or TV series" className="input" />
+    <img src="./src/assets/search (2).png" alt="" className="images" />
+      <h1 className="text-[32px] font-normal leading-10 tracking-[-0.5px] text-left h1 " >
+        Trending
+      </h1>
+      <div className="main_images ">
+      <img src="./src/assets/Rectangle (6).png" alt="png" />
+      <img src="./src/assets/Rectangle Copy 2 (2).png" alt="png" />
+         </div>
+      <img src="./src/assets/Group 27.png" alt="png"  className="card1"/>
+      <img src="./src/assets/Group 27.png" alt="png"  className="card2"/>
+    <div className="main">
+      <h1 className="section">Beyond Earth</h1>
+      <h1 className="section1">Bottom Gear</h1>
+    </div>
+    <h1 className="main_win">Recommended for you</h1>
+    <div className="container1">
+    <div className="card">
+      <img src="./src/assets/Rectangle (1).png" alt="png"  />
+      <h1 className="h1">No Land Beyond</h1>
+      <img src="./src/assets/Group 27.png" alt="png" className="m"/>
+          <div className="p">
+            <p>2019</p>
+            <p>movies</p>
+            <p>PG</p>
+          </div>
+    </div>
+
+   
+    <div className="card">
+      <img src="./src/assets/Rectangle (1).png" alt="png"  />
+      <h1 className="h1">No Land Beyond</h1>
+      <img src="./src/assets/Group 27.png" alt="png" className="m"/>
+          <div className="p">
+            <p>2019</p>
+            <p>movies</p>
+            <p>PG</p>
+          </div>
+    </div>
+    <div className="card">
+      <img src="./src/assets/Rectangle (1).png" alt="png"  />
+      <h1 className="h1">No Land Beyond</h1>
+      <img src="./src/assets/Group 27.png" alt="png" className="m"/>
+          <div className="p">
+            <p>2019</p>
+            <p>movies</p>
+            <p>PG</p>
+          </div>
+    </div>
+    <div className="card">
+      <img src="./src/assets/Rectangle (1).png" alt="png"  />
+      <h1 className="h1">No Land Beyond</h1>
+      <img src="./src/assets/Group 27.png" alt="png" className="m"/>
+          <div className="p">
+            <p>2019</p>
+            <p>movies</p>
+            <p>PG</p>
+          </div>
+    </div>
+    </div>
+    <div className="container2">
+    <div className="card">
+      <img src="./src/assets/Rectangle (1).png" alt="png"  />
+      <h1 className="h1">No Land Beyond</h1>
+      <img src="./src/assets/Group 27.png" alt="png" className="m"/>
+          <div className="p">
+            <p>2019</p>
+            <p>movies</p>
+            <p>PG</p>
+          </div>
+    </div>
+    <div className="card">
+      <img src="./src/assets/Rectangle (1).png" alt="png"  />
+      <h1 className="h1">No Land Beyond</h1>
+      <img src="./src/assets/Group 27.png" alt="png" className="m"/>
+          <div className="p">
+            <p>2019</p>
+            <p>movies</p>
+            <p>PG</p>
+          </div>
+    </div>
+    <div className="card">
+      <img src="./src/assets/Rectangle (1).png" alt="png"  />
+      <h1 className="h1">No Land Beyond</h1>
+      <img src="./src/assets/Group 27.png" alt="png" className="m"/>
+          <div className="p">
+            <p>2019</p>
+            <p>movies</p>
+            <p>PG</p>
+          </div>
+    </div>
+    <div className="card">
+      <img src="./src/assets/Rectangle (1).png" alt="png"  />
+      <h1 className="h1">No Land Beyond</h1>
+      <img src="./src/assets/Group 27.png" alt="png" className="m"/>
+          <div className="p">
+            <p>2019</p>
+            <p>movies</p>
+            <p>PG</p>
+          </div>
+    </div>
+    </div>
+    <div className="container3">
+    <div className="card">
+      <img src="./src/assets/Rectangle (1).png" alt="png"  />
+      <h1 className="h1">No Land Beyond</h1>
+      <img src="./src/assets/Group 27.png" alt="png" className="m"/>
+          <div className="p">
+            <p>2019</p>
+            <p>movies</p>
+            <p>PG</p>
+          </div>
+    </div>
+    <div className="card">
+      <img src="./src/assets/Rectangle (1).png" alt="png"  />
+      <h1 className="h1">No Land Beyond</h1>
+      <img src="./src/assets/Group 27.png" alt="png" className="m"/>
+          <div className="p">
+            <p>2019</p>
+            <p>movies</p>
+            <p>PG</p>
+          </div>
+    </div>
+    <div className="card">
+      <img src="./src/assets/Rectangle (1).png" alt="png"  />
+      <h1 className="h1">No Land Beyond</h1>
+      <img src="./src/assets/Group 27.png" alt="png" className="m"/>
+          <div className="p">
+            <p>2019</p>
+            <p>movies</p>
+            <p>PG</p>
+          </div>
+    </div>
+    <div className="card">
+      <img src="./src/assets/Rectangle (1).png" alt="png"  />
+      <h1 className="h1">No Land Beyond</h1>
+      <img src="./src/assets/Group 27.png" alt="png" className="m"/>
+          <div className="p">
+            <p>2019</p>
+            <p>movies</p>
+            <p>PG</p>
+          </div>
+    </div>
+    </div>
+    <div className="container4">
+    <div className="card">
+      <img src="./src/assets/Rectangle (1).png" alt="png"  />
+      <h1 className="h1">No Land Beyond</h1>
+      <img src="./src/assets/Group 27.png" alt="png" className="m"/>
+          <div className="p">
+            <p>2019</p>
+            <p>movies</p>
+            <p>PG</p>
+          </div>
+    </div>
+    <div className="card">
+      <img src="./src/assets/Rectangle (1).png" alt="png"  />
+      <h1 className="h1">No Land Beyond</h1>
+      <img src="./src/assets/Group 27.png" alt="png" className="m"/>
+          <div className="p">
+            <p>2019</p>
+            <p>movies</p>
+            <p>PG</p>
+          </div>
+    </div>
+    <div className="card">
+      <img src="./src/assets/Rectangle (1).png" alt="png"  />
+      <h1 className="h1">No Land Beyond</h1>
+      <img src="./src/assets/Group 27.png" alt="png" className="m"/>
+          <div className="p">
+            <p>2019</p>
+            <p>movies</p>
+            <p>PG</p>
+          </div>
+    </div>
+    <div className="card">
+      <img src="./src/assets/Rectangle (1).png" alt="png"  />
+      <h1 className="h1">No Land Beyond</h1>
+      <img src="./src/assets/Group 27.png" alt="png" className="m"/>
+          <div className="p">
+            <p>2019</p>
+            <p>movies</p>
+            <p>PG</p>
+          </div>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default HomeView;
